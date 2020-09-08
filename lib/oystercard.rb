e7d416e95506cfe078c7d0cff7a6a356d227d25d
Veronica Lee <veronicavlee@icloud.com>
2020-09-08 15:56:46 +0100

    Update code and test for  max limit on card balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
   attr_reader :balance
   DEFAULT_BALANCE = 0
+  MAX_LIMIT = 90
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
 
   def top_up(amount)
+    raise "Exceeds maximum card limit of #{MAX_LIMIT}" if (@balance + amount) > MAX_LIMIT
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,7 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) { Oystercard.new }
+  let(:amount) { 20 }
 
   it 'sets a balance in the card with default value of 0' do
     expect(card.balance).to eq(0)
@@ -11,13 +12,21 @@ describe Oystercard do
   describe '#top_up()' do
     # so we know something is passed to the method
     it 'increases the balance of the card by amount' do
-      amount = 100
       expect{ card.top_up(amount) }.to change{card.balance}.by(amount)
     end
 
     it 'returns the updated balance of the card' do
-      amount = 100
       expect(card.top_up(amount)).to eq(card.balance)
+    end
+
+    it 'raises an error if amount is above the maximum limit' do
+      amount = Oystercard::MAX_LIMIT * 2
+      expect{ card.top_up(amount) }.to raise_error("Exceeds maximum card limit of #{Oystercard::MAX_LIMIT}")
+    end
+
+    it 'raises an error if the top up would increase the balance over the maximum limit' do
+      card.top_up(Oystercard::MAX_LIMIT) # set up test card with a full balance
+      expect{ card.top_up(1) }.to raise_error("Exceeds maximum card limit of #{Oystercard::MAX_LIMIT}")
     end
   end
 
