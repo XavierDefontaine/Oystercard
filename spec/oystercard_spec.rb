a700e9ed185ded0043cd399e83b84945aff999f4
Veronica Lee <veronicavlee@icloud.com>
2020-09-08 15:39:04 +0100

    Update top_up to increase the balance Refactor top_up tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,24 @@
 require 'oystercard'
 
 describe Oystercard do
-    it 'sets a balance in the card with default value of 0' do
-        card = Oystercard.new
-        expect(card.balance).to eq(0)
-    end
-    
-    describe '#top_up()' do # so we know something is passed to the method
-      it 'increases the balance of the card by amount' do
-        card = Oystercard.new
-        expect(card.top_up(100)).to eq(100)
-      end
+
+  subject(:card) { Oystercard.new }
+
+  it 'sets a balance in the card with default value of 0' do
+    expect(card.balance).to eq(0)
+  end
+
+  describe '#top_up()' do
+    # so we know something is passed to the method
+    it 'increases the balance of the card by amount' do
+      amount = 100
+      expect{ card.top_up(amount) }.to change{card.balance}.by(amount)
     end
 
+    it 'returns the updated balance of the card' do
+      amount = 100
+      expect(card.top_up(amount)).to eq(card.balance)
+    end
+  end
 
 end
