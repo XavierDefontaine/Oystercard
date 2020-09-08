2f5761283cd14fbaa75b598eee5291f8c8e7bb82
cristina <c.fujiname@gmail.com>
2020-09-08 15:27:23 +0100

    add test for top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,5 +5,13 @@ describe Oystercard do
         card = Oystercard.new
         expect(card.balance).to eq(0)
     end
+    
+    describe '#top_up()' do # so we know something is passed to the method
+      it 'increases the balance of the card by amount' do
+        card = Oystercard.new
+        expect(card.top_up(100)).to eq(100)
+      end
+    end
+
 
 end
