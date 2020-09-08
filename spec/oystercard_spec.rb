a35a8b0d19fe0226d0c6ecdad664a00eed009120
cristina <c.fujiname@gmail.com>
2020-09-08 15:18:32 +0100

    Add a balance attribute, refactored

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+    it 'sets a balance in the card with default value of 0' do
+        card = Oystercard.new
+        expect(card.balance).to eq(0)
+    end
 
 end
